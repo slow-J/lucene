cd689261f2094770ffb129d21bb81e6fcd40fbda
Jakub Slowinski <jslowins@amazon.com>
2023-08-24 22:33:04 +0100

    Adding and providing Test: codec, postingformat, postingwriter, postingreader
    Forking PForUtil as ForUtilNoP.java which is identical, minus all of the exclusions.
    
    Doing this to test performance impact of turning off patching in Lucene's PFOR encoding

--- lucene/core/src/java/module-info.java
+++ lucene/core/src/java/module-info.java
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-import org.apache.lucene.codecs.lucene95.Lucene95Codec;
+import org.apache.lucene.codecs.lucene95.LuceneTestCodec;
 import org.apache.lucene.codecs.lucene95.Lucene95HnswVectorsFormat;
 
 /** Lucene Core. */
@@ -64,13 +64,13 @@ module org.apache.lucene.core {
   provides org.apache.lucene.analysis.TokenizerFactory with
       org.apache.lucene.analysis.standard.StandardTokenizerFactory;
   provides org.apache.lucene.codecs.Codec with
-      Lucene95Codec;
+      LuceneTestCodec;
   provides org.apache.lucene.codecs.DocValuesFormat with
       org.apache.lucene.codecs.lucene90.Lucene90DocValuesFormat;
   provides org.apache.lucene.codecs.KnnVectorsFormat with
       Lucene95HnswVectorsFormat;
   provides org.apache.lucene.codecs.PostingsFormat with
-      org.apache.lucene.codecs.lucene90.Lucene90PostingsFormat;
+      org.apache.lucene.codecs.lucene90.LuceneTestPostingsFormat;
   provides org.apache.lucene.index.SortFieldProvider with
       org.apache.lucene.search.SortField.Provider,
       org.apache.lucene.search.SortedNumericSortField.Provider,
